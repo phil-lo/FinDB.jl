719c1b2590f8ccffeeef08dd36416f8e25ccb1c9
Philippe Lacroix-Ouellette <philippe.lacroix-ouellette@hec.ca>
2023-03-07 13:52:44 -0500

    update

--- src/FinDB.jl
+++ src/FinDB.jl
@@ -1,8 +1,12 @@
 module FinDB
 
+using YFinance
+using DataFrames
+using Dates
+
 include("datasources/sources.jl")
 include("datasources/yahoo.jl")
 
-export prices
+export prices, Yahoo
 
 end # module FinDB
